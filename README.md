547bee06cdbbb694305b9515f1fc3d61ad209ab4
Tom Moor <tom.moor@gmail.com>
2016-12-31 07:18:34 +0000

    Updated to latest API version

--- README.md
+++ README.md
@@ -7,16 +7,39 @@ PageMunch is a simple API backed by an intelligent web crawler, that lets you ex
 
 
 ## Installation
- 
+
 You can install the PHP module using Composer, simply add it to your requirements, for example:
 
     {
         "require": {
-            "PageMunch/PageMunch": ">=0.1.0"
+            "PageMunch/PageMunch": ">=1.0.0"
         }
     }
 
 Or clone the repository directly from GitHub and use the class in the vendors directory of your Framework or elsewhere.
+
+## Usage
+
+```php
+// if using Composer then you should use an autoloader, otherwise require the
+// file however you might normally
+require('src/PageMunch/PageMunch.php');
+use PageMunch\PageMunch;
+
+// create a new API object to use for requests - your API key from the PageMunch
+// account dashboard should be passed in here. We recommend using an environment
+// variable to make distributing code between environments easier and more secure
+$api = new PageMunch(array('key' => getenv('PAGEMUNCH_API_KEY')));
+
+$response = $api->extract('http://www.youtube.com/watch?v=9bZkp7q19f0');
+
+// check if the API call succeeded and output the title from the response data
+if ($response) {
+	print_r($response->title);
+} else {
+	print_r($api->getError()->message);
+}
+```
 
 
 ## More Details
--- src/PageMunch/PageMunch.php
+++ src/PageMunch/PageMunch.php
@@ -10,18 +10,17 @@
 namespace PageMunch;
 
 class PageMunch {
-	
+
 	/**
 	* The library version, will be provided in the request headers.
 	*/
-	const VERSION = '0.1.0';
-	
+	const VERSION = '1.0.0';
+
 	private $config = array();
 	public $response = array();
-	
-	
+
 	public function __construct(array $config = array()) {
-	
+
 		$this->config = array_merge(array(
 			'host' => 'api.pagemunch.com',
 			'version' => '1',
@@ -32,8 +31,8 @@ class PageMunch {
 			'curl_timeout' => 5
 		), $config);
 	}
-	
-	
+
+
 	/**
 	 * Requests a summary of the URL
 	 *
@@ -41,51 +40,36 @@ class PageMunch {
 	 * @param  array   $options Additional query parameters
 	 * @return Mixed   Returns a response object on success, otherwise false.
 	 */
-	public function summary($url, array $options = array()) {
-		
-		return $this->get('summary', array_merge(array(
+	public function extract($url, array $options = array()) {
+
+		return $this->get('extract', array_merge(array(
 			'url' => $url
 		), $options));
 	}
-	
-	
-	/**
-	 * Requests a classification of the URL
-	 *
-	 * @param  string  $url     The url to parse
-	 * @param  array   $options Additional query parameters
-	 * @return Mixed   Returns a response object on success, otherwise false.
-	 */
-	public function classify($url, array $options = array()) {
-		
-		return $this->get('classify', array_merge(array(
-			'url' => $url
-		), $options));
-	}
-	
-	
+
+
 	/**
 	 * Constructs a full URI to make an API request including
-	 * the account key.
+	 * the API key.
 	 *
 	 * @param  string  $method   The name of an API endpoint
 	 * @param  array   $options  Additional query parameters
 	 * @return Mixed   Returns a response object on success, otherwise false.
 	 */
 	public function get($method, array $options = array()) {
-		
-		$options['key'] = $this->config['key'];
-		
-		$url = 'http://'. $this->config['host'] .'/'. $this->config['version'] .'/'. $method .'.'. $this->config['format'] .'?'. http_build_query($options);
-		
+
+		$options['apiKey'] = $this->config['key'];
+
+		$url = 'https://'. $this->config['host'] . '/'. $method .'?'. http_build_query($options);
+
 		if ($this->request($url)) {
 			return $this->getResponse();
 		}
-		
+
 		return false;
 	}
-	
-	
+
+
 	/**
 	 * Once a request to the API has been made, this method will
 	 * return the full decoded response object of the last call.
@@ -95,8 +79,8 @@ class PageMunch {
 	public function getResponse() {
 		return $this->response['response'];
 	}
-	
-	
+
+
 	/**
 	 * In the event that an API request fails, calling this method
 	 * will return the error object with details on the failure.
@@ -106,17 +90,17 @@ class PageMunch {
 	public function getError() {
 		return $this->response['response'] ?: $this->response['error'];
 	}
-	
-	
+
+
 	/**
-	 * Performs a curl request to the PageMunch API and gathers the 
+	 * Performs a curl request to the PageMunch API and gathers the
 	 * response information into a single parsed object.
 	 *
 	 * @param  string  $url    The fully qualified path of an API request
 	 * @return boolean  Whether or not the request succeeded.
 	 */
 	private function request($url) {
-		
+
 		// configure curl
 		$c = curl_init();
 		curl_setopt_array($c, array(
@@ -130,7 +114,7 @@ class PageMunch {
 			CURLOPT_HEADER         => false,
 			CURLINFO_HEADER_OUT    => true,
 		));
-		
+
 		// perform request
 		$response = curl_exec($c);
 		$status_code = (int) curl_getinfo($c, CURLINFO_HTTP_CODE);
@@ -138,13 +122,13 @@ class PageMunch {
 		$error = curl_error($c);
 		$errno = curl_errno($c);
 		curl_close($c);
-		
+
 		// store the response
 		$this->response['status_code'] = $status_code;
 		$this->response['info'] = $info;
 		$this->response['error'] = $error;
 		$this->response['errno'] = $errno;
-		
+
 		try {
 			$this->response['response'] = json_decode($response);
 		} catch(Exception $e) {
@@ -153,7 +137,7 @@ class PageMunch {
 				'message' => 'Could not parse JSON response'
 			);
 		}
-		
+
 		return $status_code === 200;
 	}
 }
